4746c19a19d8e785876441975e287b0eec0ace34
Damien Nelson <dhnisaman@gmail.com>
2023-04-06 23:40:31 -0400

    commit tree a021027c307a21c121a3f81495923f8fd5827b08

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,21 @@
 class ApplicationController < Sinatra::Base
+  set :default_content_type, 'application/json'
+  
+  get "/bakeries" do
+    Bakery.all.to_json
+  end
 
-  # add routes
+  get "/bakeries/:id" do
+    Bakery.find(params[:id]).to_json(include: :baked_goods)
+  end
+
+  get '/baked_goods/by_price' do
+    BakedGood.order(price: :desc).to_json
+  end
+
+  get '/baked_goods/most_expensive' do
+    BakedGood.order(price: :desc).first.to_json
+    # BakedGood.order(price: :desc).limit(1).to_json
+  end
 
 end
